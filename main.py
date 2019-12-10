e11c7fc4d76a176e5539e72b175468e1c23fa40d
Diva Krishna <divakrishnam@gmail.com>
2019-12-10 09:20:49 +0700

    update

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
 import pertama
-
-pertama = pertama.Pertama()
-
-#Kadek Diva Krishna Murti - 1174006
-pertama.ruangan111('Ruangan 111')
-#Kadek Diva Krishna Murti - 1174006
-pertama.wcBL('WC Belakang Kiri')
-#Kadek Diva Krishna Murti - 1174006
-pertama.tanggaBL('Tangga Belakang Kiri')
-#Kadek Diva Krishna Murti - 1174006
-pertama.gudangBL('Gudang Belakang Kiri')
-#Kadek Diva Krishna Murti - 1174006
-pertama.haloPos('Halo Pos 161')
-
-pertama.close()

e69fbd2fe46756e2aa958948f59679d37eb880eb
Diva Krishna <divakrishnam@gmail.com>
2019-12-10 09:30:07 +0700

    1174006-KadekDivaKrishnaMurti

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import pertama
+
+pertama = pertama.Pertama()
+
+#Kadek Diva Krishna Murti - 1174006
+pertama.ruangan111('Ruangan 111')
+#Kadek Diva Krishna Murti - 1174006
+pertama.wcBL('WC Belakang Kiri')
+#Kadek Diva Krishna Murti - 1174006
+pertama.tanggaBL('Tangga Belakang Kiri')
+#Kadek Diva Krishna Murti - 1174006
+pertama.gudangBL('Gudang Belakang Kiri')
+#Kadek Diva Krishna Murti - 1174006
+pertama.haloPos('Halo Pos 161')
+
+pertama.close()

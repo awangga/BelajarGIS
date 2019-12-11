1daa67bc6ba8e2b9b969a6fdf09b5efae12e6a2e
Bakti Qilan <bakti.qilan17@gmail.com>
2019-12-11 15:27:03 +0700

    1174083

--- ketiga.py
+++ ketiga.py
@@ -2,49 +2,56 @@ import shapefile
 
 
 class ketiga:
-    
+
     def __init__(self):
         self.ketiga = shapefile.Writer('ketiga', shapeType=shapefile.POLYGON)
         self.ketiga.shapeType
         self.ketiga.field('nama_ruangan', 'C')
 
 #-------------------- KODING ------------------#
-    
-    #Ilham Muhammad Ariq 1174087
+
+    # Ilham Muhammad Ariq 1174087
     def tanggaD2(self, nama):
         self.ketiga.record(nama)
-        self.ketiga.poly([[[-16, 20], [-19, 20], [-19, 27], [-16, 27], [-16, 20]]])
+        self.ketiga.poly(
+            [[[-16, 20], [-19, 20], [-19, 27], [-16, 27], [-16, 20]]])
 
     def r301(self, nama):
         self.ketiga.record(nama)
-        self.ketiga.poly([[[-12.4, 20], [-16, 20], [-16, 24], [-12.4, 24], [-12.4, 20]]])
+        self.ketiga.poly(
+            [[[-12.4, 20], [-16, 20], [-16, 24], [-12.4, 24], [-12.4, 20]]])
 
+    # Alvan Alvanzah 1174077
 
-    #Alvan Alvanzah 1174077
     def r302(self, nama):
         self.ketiga.record(nama)
-        self.ketiga.poly([[[-8.8, 20], [-12.4, 20], [-12.4, 24], [-8.8, 24], [-8.8, 20]]])
+        self.ketiga.poly(
+            [[[-8.8, 20], [-12.4, 20], [-12.4, 24], [-8.8, 24], [-8.8, 20]]])
 
     # Advent Nopele Sihite 1184089
     def r304(self, nama):
         self.ketiga.record(nama)
-        self.ketiga.poly([[[-1.6, 20], [-5.2, 20], [-5.2, 24], [-1.6, 24], [-1.6, 20]]])
-	
-	#Difa 
+        self.ketiga.poly(
+            [[[-1.6, 20], [-5.2, 20], [-5.2, 24], [-1.6, 24], [-1.6, 20]]])
+
+        # Difa
     def r303(self, nama):
         self.ketiga.record(nama)
-        self.ketiga.poly([[[-5.2, 20], [-8.8, 20], [-8.8, 24], [-5.2, 24], [-5.2, 20]]])
-    
-    #Muhammad Reza Syachrani 1174084
+        self.ketiga.poly(
+            [[[-5.2, 20], [-8.8, 20], [-8.8, 24], [-5.2, 24], [-5.2, 20]]])
+
+    # Muhammad Reza Syachrani 1174084
     def r307(self, nama):
         self.ketiga.record(nama)
-        self.ketiga.poly([[[9.2, 20], [5.6, 20], [5.6, 24], [9.2, 24], [9.2, 20]]])
-    
+        self.ketiga.poly(
+            [[[9.2, 20], [5.6, 20], [5.6, 24], [9.2, 24], [9.2, 20]]])
+
     def r308(self, nama):
         self.ketiga.record(nama)
-        self.ketiga.poly([[[12.8, 20], [9.2, 20], [9.2, 24], [12.8, 24], [12.8, 20]]])
+        self.ketiga.poly(
+            [[[12.8, 20], [9.2, 20], [9.2, 24], [12.8, 24], [12.8, 20]]])
 
-    #Kaka Kamaludin 1174067
+    # Kaka Kamaludin 1174067
     def r305(self, nama):
         self.ketiga.record(nama)
         self.ketiga.poly([[[2, 20], [-1.6, 20], [-1.6, 24], [2, 24], [2, 20]]])
@@ -52,40 +59,50 @@ class ketiga:
     def r306(self, nama):
         self.ketiga.record(nama)
         self.ketiga.poly([[[5.6, 20], [2, 20], [2, 24], [5.6, 24], [5.6, 20]]])
-    
-    #Arrizal Furqona Gifary 1174070
+
+    # Arrizal Furqona Gifary 1174070
     def r309(self, nama):
         self.ketiga.record(nama)
-        self.ketiga.poly([[[16.4, 20], [12.8, 20], [12.8, 24], [16.4, 24], [16.4, 20]]])
-        
-    #Fanny Shafira 1174069
+        self.ketiga.poly(
+            [[[16.4, 20], [12.8, 20], [12.8, 24], [16.4, 24], [16.4, 20]]])
+
+    # Fanny Shafira 1174069
     def r310(self, nama):
         self.ketiga.record(nama)
-        self.ketiga.poly([[[20, 20], [16.4, 20], [16.4, 24], [20, 24], [20, 20]]])
+        self.ketiga.poly(
+            [[[20, 20], [16.4, 20], [16.4, 24], [20, 24], [20, 20]]])
 
-    #Chandra Kirana Poetra 1174079
+    # Chandra Kirana Poetra 1174079
     def rwccewek2(self, nama):
         self.ketiga.record(nama)
         self.ketiga.poly([[[22, 20], [20, 20], [20, 24], [22, 24], [22, 20]]])
-   
+
     def rwccewek3(self, nama):
         self.ketiga.record(nama)
         self.ketiga.poly([[[24, 20], [22, 20], [22, 24], [24, 24], [24, 20]]])
 
-    #Mochamad Arifqi Ramadhan 1174074
+    # Mochamad Arifqi Ramadhan 1174074
     def tanggaB2(self, nama):
         self.ketiga.record(nama)
         self.ketiga.poly([[[27, 20], [24, 20], [24, 27], [27, 27], [27, 20]]])
-    
-    #Handi Handi Hermawan 1174080
+
+    # Handi Handi Hermawan 1174080
     def r311(self, nama):
         self.ketiga.record(nama)
         self.ketiga.poly([[[16, 12], [16, 18], [22, 18], [22, 12], [16, 12]]])
 
-    #lanjutkan
+    # Bakti Qilan Mufid 1174083
+    def r312(self, nama):
+        self.ketiga.record(nama)
+        self.ketiga.poly([[[16, 6], [16, 12], [22, 12], [22, 6], [16, 6]]])
+
+    def tanggaB1(self, nama):
+        self.ketiga.record(nama)
+        self.ketiga.poly([[[27, -3], [24, -3], [24, 4], [27, 4], [27, -3]]])
+
+    # lanjutkan
 
 #-------------------- BATAS END KODING ------------------#
 
     def close(self):
         self.ketiga.close()
-        
--- main.py
+++ main.py
@@ -1,141 +1,140 @@
+import ketiga
+import kedua
 import pertama
 
 pertama = pertama.Pertama()
 
-#Harun Ar - Rasyid - 1174027
+# Harun Ar - Rasyid - 1174027
 pertama.baak('BAAK')
-#Harun Ar - Rasyid - 1174027
+# Harun Ar - Rasyid - 1174027
 pertama.bauk('BAUK')
-#Kadek Diva Krishna Murti - 1174006
+# Kadek Diva Krishna Murti - 1174006
 pertama.ruangan111('Ruangan 111')
-#Nico Ekklesia Sembiring - 1174096
+# Nico Ekklesia Sembiring - 1174096
 pertama.ruangan113('Ruangan 113')
-#Kadek Diva Krishna Murti - 1174006
+# Kadek Diva Krishna Murti - 1174006
 pertama.wcBL('WC Belakang Kiri')
-#Kadek Diva Krishna Murti - 1174006
+# Kadek Diva Krishna Murti - 1174006
 pertama.tanggaBL('Tangga Belakang Kiri')
-#Kadek Diva Krishna Murti - 1174006
+# Kadek Diva Krishna Murti - 1174006
 pertama.gudangBL('Gudang Belakang Kiri')
-#Kadek Diva Krishna Murti - 1174006
+# Kadek Diva Krishna Murti - 1174006
 pertama.haloPos('Halo Pos 161')
-#Dwi Yulianingsih - 1174009
+# Dwi Yulianingsih - 1174009
 pertama.ruang117('Ruang 117')
-#Dwi Septiani Tsaniyah - 1174003
+# Dwi Septiani Tsaniyah - 1174003
 pertama.ruang115('ruang 115')
-#Habib Abdul R - 1174002
+# Habib Abdul R - 1174002
 pertama.ruang103('ruang 103')
-#Felix Lase - 1174026
+# Felix Lase - 1174026
 pertama.ruang107('RUANG 107')
 pertama.ruang108('RUANG 108')
-#Evietania Charis Sujadi - 1174051
+# Evietania Charis Sujadi - 1174051
 pertama.gudangL('GUDANG L')
 pertama.tanggaL('TANGGA L')
-#Muhammad Dzihan Al-Banna - 1174095
+# Muhammad Dzihan Al-Banna - 1174095
 pertama.ruang116('RUANG 116')
-#Arjun Yuda Firwanda 1174008
+# Arjun Yuda Firwanda 1174008
 pertama.gudangR('GUDANG R')
 pertama.tanggaR('TANGGA R')
 
-#Dezha Aidil Martha 1174025
+# Dezha Aidil Martha 1174025
 pertama.ruang101('RUANG 101')
 
-#Damara Bendikta 1174012
+# Damara Bendikta 1174012
 pertama.ruang102('RUANG 102')
 
-#Oniwaldus Bere Mali 1174005
+# Oniwaldus Bere Mali 1174005
 pertama.ruang104('RUANG 104')
 
-#Srirahayu 1174015
+# Srirahayu 1174015
 pertama.ruang105('RUANG 105')
 
-#Muhammad Tomy Nur Maulidy 1174031
+# Muhammad Tomy Nur Maulidy 1174031
 pertama.ruang106('RUANG 106')
 
-#Choirul anam 117404
+# Choirul anam 117404
 pertama.ruang114('RUANG 114')
 
-#Muh. Rifky Prananda 1174017
+# Muh. Rifky Prananda 1174017
 pertama.ruang112('RUANG 112')
 
-#Muhammad Fahmi - 1174021
+# Muhammad Fahmi - 1174021
 pertama.ruang109('RUANG 109')
-#Muhammad Fahmi - 1174021
+# Muhammad Fahmi - 1174021
 pertama.wcBL2('WC Belakang Kanan')
-#Muhammad Fahmi - 1174021
+# Muhammad Fahmi - 1174021
 pertama.tanggaBL2('Tangga Belakang Kanan')
-#Muhammad Fahmi - 1174021
+# Muhammad Fahmi - 1174021
 pertama.gudangBL2('Gudang Belakang Kanan')
-
 
 
 pertama.close()
 
-#Kelas B
-import kedua
+# Kelas B
 
 kedua = kedua.Kedua()
 
-#Luthfi Muhammad Nabil - 1174035
+# Luthfi Muhammad Nabil - 1174035
 kedua.tanggaBawahKiri('Tangga Bawah Kiri')
-#Luthfi Muhammad Nabil - 1174035
+# Luthfi Muhammad Nabil - 1174035
 kedua.tanggaBawahKanan('Tangga Bawah Kanan')
-#Luthfi Muhammad Nabil - 1174035
+# Luthfi Muhammad Nabil - 1174035
 kedua.tanggaAtasKanan('Tangga Atas Kanan')
-#Luthfi Muhammad Nabil - 1174035
+# Luthfi Muhammad Nabil - 1174035
 kedua.tanggaAtasKiri('Tangga Atas Kiri')
-#Luthfi Muhammad Nabil - 1174035
+# Luthfi Muhammad Nabil - 1174035
 kedua.tamanKosongTengah('Taman Kosong Tengah')
-#Hagan Rowlenstino - 1174040
+# Hagan Rowlenstino - 1174040
 kedua.R213('Ruangan_213')
-#Hagan Rowlenstino - 1174040
+# Hagan Rowlenstino - 1174040
 kedua.IRC('Ruangan_IRC')
-#Hagan Rowlenstino - 1174040
+# Hagan Rowlenstino - 1174040
 kedua.RLabBisnis('Ruangan_Lab_Bisnis')
-#Hagan Rowlenstino - 1174040
+# Hagan Rowlenstino - 1174040
 kedua.RLabComprehensive('Ruangan_Labkom_Comprehensive')
-#Irvan Rizkiansyah - 1174043
+# Irvan Rizkiansyah - 1174043
 kedua.ruangan205('Ruangan 205')
-#Irvan Rizkiansyah - 1174043
+# Irvan Rizkiansyah - 1174043
 kedua.ruangan206('Ruangan 206')
-#Irvan Rizkiansyah - 1174043
+# Irvan Rizkiansyah - 1174043
 kedua.ruangan207('Ruangan 207')
-#Liyana Majdah Rahma - 1174039
+# Liyana Majdah Rahma - 1174039
 kedua.ruangan201('Ruangan 201')
-#Liyana Majdah Rahma - 1174039
+# Liyana Majdah Rahma - 1174039
 kedua.ruangan202('Ruangan 202')
-#Alit Fajar Kurniawan - 1174057
+# Alit Fajar Kurniawan - 1174057
 kedua.RServer('Ruangan Server')
-#Alit Fajar Kurniawan - 1174057
+# Alit Fajar Kurniawan - 1174057
 kedua.LabLogistik('Laboratorium Logistik')
 
 
-#Kevin Natanael Nainggolan-1174059
+# Kevin Natanael Nainggolan-1174059
 kedua.ruang208('Ruangan 208')
-#Kevin Natanael Nainggolan-1174059
+# Kevin Natanael Nainggolan-1174059
 kedua.ruang209('Ruangan 209')
-#Kevin Natanael Nainggolan-1174059
+# Kevin Natanael Nainggolan-1174059
 kedua.ruang210('Ruangan 210')
-#Dika Sukma Pradana - 1174050
+# Dika Sukma Pradana - 1174050
 kedua.ruangan219('Ruangan 219')
-#Dika Sukma Pradana - 1174050
+# Dika Sukma Pradana - 1174050
 kedua.ruangan220('Ruangan 220')
 
-#Faisal Najib Abdullah - 1174042
+# Faisal Najib Abdullah - 1174042
 kedua.toiletdosen('Toilet Dosen')
 kedua.toiletcowo('Toilet Cowo')
 kedua.prodiak('Prodi AK')
 
-#Rangga Putra Ramdhani - 1174056
+# Rangga Putra Ramdhani - 1174056
 kedua.ruangan203('Ruangan 203')
 kedua.ruangan204('Ruangan 204')
-#Selanjutnya disini
+# Selanjutnya disini
 
 
-#Close Jangan DIhapus
+# Close Jangan DIhapus
 kedua.close()
 
-#KelasC
-import ketiga
+# KelasC
 ketiga = ketiga.ketiga()
 #-------------------KODING --------------------------#
 
@@ -177,5 +176,10 @@ ketiga.tanggaB2('Tangga Belakang 2')
 
 # Handi Hermawan 1174080
 ketiga.r311('Ruangan 311')
-#lanjutkan
+
+# Bakti Qilan Mufid 1174083
+ketiga.r312('Ruangan 312')
+ketiga.tanggaB1('Tangga Belakang 1')
+
+# lanjutkan
 ketiga.close()

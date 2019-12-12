59d1639eb70acbc2d2700c995e3c5cf8a302a3df
afrafaris72 <afra.faris123@gmail.com>
2019-12-12 16:02:07 +0700

    1174041

--- main.py
+++ main.py
@@ -131,10 +131,12 @@ kedua.ruangan204('Ruangan 204')
 
 #Ichsan Hizman Hardy - 1174034
 kedua.ruangan221('Ruangan 221')
+#Ichsan Hizman Hardy - 1174034
 kedua.ruangan222('Ruangan 222')
 
 # Muhammad Afra Faris - 1174041
 kedua.ruangan211('Ruangan 211')
+# Muhammad Afra Faris - 1174041
 kedua.ruangan212('Ruangan 212')
 # Selanjutnya disini
 

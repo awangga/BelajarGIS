af674ed74da3ba22617d3ca980ee89b69ff19e77
Arrizal <solehizal@gmail.com>
2020-01-07 07:49:51 +0700

    1174070

--- main.py
+++ main.py
@@ -1151,6 +1151,47 @@ ciwidey.close()
 #############  Dini Permata Putri      #############
 #############          Selesai         #############
 ####################################################
+
+#---------------------   Mulai Quiz 3    ------------------------#
+#-------------------    Arrizal Furqona gifary     -------------------#
+import cimenyan
+cimenyan = cimenyan.Cimenyan()
+
+# Kelurahan
+cimenyan.kelurahanMekarsari('Mekarsari')
+cimenyan.kelurahanCiburial('Ciburial')
+cimenyan.kelurahanCibenying('Cibenying')
+cimenyan.kelurahanCikadut('Cikadut')
+cimenyan.kelurahanMandalamekar('Mandalamekar')
+cimenyan.kelurahanSindanglaya('Sindanglaya')
+cimenyan.kelurahanMekarmanik('Mekarmanik')
+cimenyan.kelurahanCimenyan('Cimenyan')
+cimenyan.kelurahanPadasuka('Padasuka')
+cimenyan.kelurahanMekarsaluyu('Mekarsaluyu')
+
+# Kantor
+cimenyan.kantorKelurahanCibenying('kantor Kelurahan Cibenying')
+cimenyan.kantorKelurahanCikadut('kantor Kelurahan Cikadut')
+cimenyan.kantorKelurahanMandalamekar(' kantor Kelurahan Mandalamekar')
+cimenyan.kantorKelurahanSindanglaya('kantor Kelurahan Sindanglaya')
+cimenyan.kantorKelurahanMekarmanik('kantor Kelurahan Mekarmanik')
+cimenyan.kantorKelurahanCimenyan('kantor Kelurahan cimenyan')
+cimenyan.kantorKelurahanCiburial('kantor Kelurahan Ciburial')
+cimenyan.kantorKelurahanMekarsaluyu('kantor Kelurahan Mekarsaluyu')
+cimenyan.kantorKelurahanPadasuka('kantor Kelurahan Padasuka')
+
+# jalan
+cimenyan.jalanCiburial(' Jl. Ciburial - Mekarsaluyu ')
+cimenyan.jalanMekarsaluyu(' Jl. Mekarsaluyu - Cibenying')
+cimenyan.jalanCibenying(' Jl. Cibenying - cimenyan')
+cimenyan.jalanCimenyan(' Jl. cimenyan - Mandalamekar')
+cimenyan.jalanMandalamekar(' Jl. Mandalamekar - Cikadut')
+cimenyan.jalanMekarmanik(' Jl. Mekarmanik - Sindanglaya')
+cimenyan.close()
+
+####################################################
+#############          Selesai         #############
+####################################################
 #--------------------------    Batas    -------------------------#
 
 
